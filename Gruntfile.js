5187985ae15a383e68c46faf7d14e68f460e3be1
saikat.mitra <saikat.mitra@snapdeal.com>
2017-01-14 16:57:26 +0530

    Initial commits for repo

---
+++ Gruntfile.js
@@ -0,0 +1,24 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        jshint: {
+            all: ['src/**/*.js', 'test/**/*.js'],
+            options: {
+                globals: {
+                    _: false,
+                    $: false,
+                    describe: false,
+                    it: false,
+                    expect: false,
+                    beforeEach: false,
+                    afterEach: false,
+                    sinon: false,
+                    jasmine: false
+                },
+                browser: true,
+                devel: true
+            }
+        }
+    });
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.registerTask('default', ['jshint']);
+};
