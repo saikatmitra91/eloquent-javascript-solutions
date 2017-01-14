26c7edc89b80e389fb9953b7a90cca9b1a590b6d
saikat.mitra <saikat.mitra@snapdeal.com>
2017-01-14 23:15:44 +0530

    travis fix

--- src/chapter 3/recursion.js
+++ src/chapter 3/recursion.js
@@ -19,7 +19,7 @@ inquirer.prompt(questions).then(function(answers) {
 function isEven(number) {
     if (number > 1 || number < 0) {
         if (number < 0) {
-            return isEven(number + 2)
+            return isEven(number + 2);
         } else {
             return isEven(number - 2);
         }
